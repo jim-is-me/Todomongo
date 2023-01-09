d5d6082f1313e6cbeb8639ef905dc5824ffb841d
Prem <75975455+jim-is-me@users.noreply.github.com>
2023-01-09 15:38:32 +0530

    Revert "2 commit"

--- routes/todos.js
+++ routes/todos.js
@@ -18,6 +18,7 @@ router.get('/:id', async(req,res) => {
 
 
 router.post('/', async(req,res) => {
+    console.log(req.body,"wht up")
     const todo = new Todo.Todo({
         title: req.body.data.title,
         description: req.body.data.description,
